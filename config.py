e6003e687bb288c018980937c89b7deec240c333
YLAdatagit <datasharing202010@gmail.com>
2025-07-06 00:24:28 +0700

    change base_dir

--- config.py
+++ config.py
@@ -1,7 +1,16 @@
+from dotenv import load_dotenv
+from pathlib import Path
+import os
+
+# Load .env from project root (one level up)
+env_path = Path(__file__).resolve().parents[0] / ".env"
+load_dotenv(dotenv_path=env_path)
+
+
 date_to_add = "2025-07-04"
 date_fo = "20250704"
 
-base_dir = "D:/D&T Project/Database/MML Data"
+base_dir = os.getenv("base_dir")
 
 rar1 = f"{base_dir}/{date_fo}.rar"
 rar2 = f"{base_dir}/ericsson_{date_fo}.rar"
